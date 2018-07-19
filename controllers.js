03af4d82dcf410ebfa5dd9b14b55bda4833facf8
hacktiv8 <hacktiv8@H8JKT009.localdomain>
2018-07-19 16:16:51 +0700

    first commit

--- controllers.js
+++ controllers.js
@@ -1,8 +1,33 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let count = setInterval(() => {
+    if (seconds >= 0){
+      displayCountdown(converter(seconds));
+      seconds --;
+    } else {
+      clearInterval(count);
+    }
+  }, 1000)
 };
+
+const converter = input => {
+  let minutes = Math.floor(input / 60)
+  let seconds = input % 60; 
+  if (minutes >= 10){
+    if (seconds >= 10){
+      return `${minutes} : ${seconds}`;
+    } else {
+      return `${minutes} : 0${seconds}`;
+    }
+  } else {
+    if (seconds >= 10){
+      return `0${minutes} : ${seconds}`;
+    } else {
+      return `0${minutes} : 0${seconds}`;
+    }
+  }
+}
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -1,11 +1,10 @@
-const clear = require("clear");
 const figlet = require("figlet");
 const chalk = require("chalk");
 
 const displayCountdown = seconds => {
-  clear();
+  console.clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.cyan(figlet.textSync(seconds)));
 };
 
 module.exports = { displayCountdown };
