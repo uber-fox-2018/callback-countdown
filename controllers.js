267bb2be065685647e30df01da1a89b62cc26374
gandryeanb <gstandrianb@gmail.com>
2018-07-19 20:17:28 +0700

    adding js file

--- controllers.js
+++ controllers.js
@@ -1,7 +1,29 @@
 const { displayCountdown } = require("./views");
 
 const timer = seconds => {
-  // Your code here...
+  let repeater = setInterval(function(){
+    let display = ``
+    let hour = Math.floor(seconds/60)
+    let second = seconds % 60
+
+    if (hour < 10) {
+      display += `0`+hour+ ` `
+    }else {
+      display += hour + ` `
+    }
+
+    if (second < 10) {
+      display += `: 0`+second + ` `
+    }else {
+      display += `: `+second + ` `
+    }
+
+    if (seconds === 0) {
+      clearInterval(repeater)
+    }
+    displayCountdown(display)
+    seconds--
+  },1000)
 };
 
 module.exports = {
