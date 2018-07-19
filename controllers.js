6b924b6c816265f69fa54946dd47b0f829432a90
harlesbayu <bayuanggaraharles@gmail.com>
2018-07-19 18:32:50 +0700

    countdown

--- controllers.js
+++ controllers.js
@@ -2,8 +2,34 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  var interval = setInterval(function(){
+      var menit = Math.floor(seconds/60)
+      var detik = seconds%60
+  
+      var viewMenit = ''
+      var viewDetik = ''
+      
+      if(menit>=10){
+        viewMenit += menit
+      }else{viewMenit = `0${menit}`}
+      if(detik>=10){
+        viewDetik += detik
+      }else{viewDetik = `0${detik}`}
+  
+      displayCountdown(`${viewMenit}:${viewDetik}`)
+  
+      seconds -= 1
+  
+      if(seconds<0){
+        clearInterval(interval)
+      }
+    },1000)
+  
 };
+
+
 
 module.exports = {
   timer
 };
+
--- views.js
+++ views.js
@@ -5,7 +5,9 @@ const chalk = require("chalk");
 const displayCountdown = seconds => {
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
-  // Your code here...
+  console.log(chalk.red(figlet.textSync("Harles Bayu A")));
+  console.log(chalk.green(figlet.textSync(seconds)));
+  
 };
 
 module.exports = { displayCountdown };
