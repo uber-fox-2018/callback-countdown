96ade3fbbff46462770164326e5f980510e935b5
Fajar Tri Cahyono <fajartc02@gmail.com>
2018-07-19 17:24:03 +0700

    complete countDown

--- controllers.js
+++ controllers.js
@@ -1,8 +1,27 @@
 const { displayCountdown } = require("./views");
-
+const sleep = require('sleep')
 const timer = seconds => {
   // Your code here...
-};
+    (seconds <= 0) ? console.clear() : convertTime(seconds)
+}
+
+function convertTime(sec) {
+  while(sec >= 0) {
+    let minute = Math.floor(sec / 60)
+    let seconds = sec % 60
+    if(seconds < 10) {
+      seconds = '0' + seconds
+    }
+    
+    if(minute < 10) {
+      minute = '0' + minute
+    }
+    displayCountdown(`${minute} : ${seconds}`)
+    sleep.sleep(1)
+    sec--
+  }
+  
+}
 
 module.exports = {
   timer
