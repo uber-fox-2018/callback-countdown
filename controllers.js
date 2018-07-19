d96121506fdbccf17e9324b2df7caa08169c542e
ridhoemgl <ridhoemgl@gmail.com>
2018-07-19 18:43:17 +0700

    callback-countdown completed

--- controllers.js
+++ controllers.js
@@ -1,9 +1,34 @@
 const { displayCountdown } = require("./views");
 
-const timer = seconds => {
-  // Your code here...
-};
+
+function timer(sec){
+  let count = setInterval(function() {
+    displayCountdown(convert(sec))
+    sec--
+      if (sec === -1){
+        clearInterval(count)
+      }
+  },500)
+}
+
+function convert(sec){
+  let minute = Math.floor(sec / 60)
+  let second = Math.floor(sec % 60)
+
+  if (minute < 10){
+    var minutes = '0' + minute
+  }else {
+    minutes = minute 
+  }
+
+  if (second < 10){
+    var seconds = '0' + second
+  }else {
+    seconds = second
+  }
+  return `${minutes} : ${seconds}`
+}
 
 module.exports = {
   timer
-};
+}
