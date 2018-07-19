2ed6de418ec4082e30aeb9bc0dc9d96b7519d8b3
HelmiYogantara <helmiyogantara@gmail.com>
2018-07-19 21:16:20 +0700

    done

--- controllers.js
+++ controllers.js
@@ -2,7 +2,33 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  var intervalId= setInterval(function(){
+    if(seconds >= 0){
+      displayCountdown(convert(seconds))
+      seconds--
+    }else{
+      clearInterval(intervalId)
+    }
+    },1000)
+
 };
+
+function convert(seconds) {
+  var menit = Math.floor(seconds/60)
+  var second = seconds%60  
+  if(menit === 0 && second < 10){
+    return `0${menit} : 0${second}`
+  }else if(second < 10 && menit !== 0){
+    return `${menit}:0${second}`
+  }else if(menit === 0 && second >= 10){
+    return `0${menit}:${second}`
+  }else{
+    return `${menit} : ${second}`
+  }
+
+}
+
+
 
 module.exports = {
   timer
--- views.js
+++ views.js
@@ -3,9 +3,13 @@ const figlet = require("figlet");
 const chalk = require("chalk");
 
 const displayCountdown = seconds => {
+  
   clear();
   console.log(chalk.cyan(figlet.textSync("Countdown App")));
+  console.log(chalk.cyan(figlet.textSync(seconds)));
+  
   // Your code here...
+
 };
 
 module.exports = { displayCountdown };
