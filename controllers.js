c34b90837291fc265b6ce72b4b8b0d11bc312bca
sbachr <sbachr0501@gmail.com>
2018-07-19 16:20:28 +0700

    Controller in Callback countDown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,13 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  displayCountdown(seconds);
+  let cb = setInterval(function(){
+    debugger;
+    seconds--;
+      displayCountdown(seconds);
+      if(seconds == 0) clearInterval(cb);
+  }, 1000);
 };
 
 module.exports = {
