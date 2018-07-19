056c0f2b6fc05519bedb656f149e3d5e03004c1f
Grayfield <grayfieldmm@outlook.com>
2018-07-19 16:40:55 +0700

    Finished callback countdown challenge.

--- controllers.js
+++ controllers.js
@@ -2,8 +2,15 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  if(seconds < 0)
+    return;
+  else {
+    setTimeout(function() {
+      displayCountdown(seconds);
+      timer(--seconds);
+    }, 1000);
+  }
 };
-
 module.exports = {
   timer
 };
