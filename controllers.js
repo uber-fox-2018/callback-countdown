2896b6aa3a2f0c3aa52e9223f63af4a646fa5e13
Andre Sudi Yudatama <andresudi@DDCBU-elee.localdomain>
2018-07-19 17:03:34 +0700

    callback countdown

--- controllers.js
+++ controllers.js
@@ -2,6 +2,24 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  var set = setInterval (() => {
+    if (seconds === 0) {
+      clearInterval(set)
+    } else {
+      seconds--
+      let minute = Math.trunc(seconds/60)
+      let sec = seconds%60
+      if (sec < 10) {
+        sec = '0' + sec 
+      } 
+
+      if (minute < 10) {
+        minute = '0' + minute
+      }
+      
+      displayCountdown(`${minute} : ${sec}`)
+    }
+  }, 200)
 };
 
 module.exports = {
