bd0b33c9faaab61351f427771847f233b35b514c
andryjns <andry_jns@yahoo.com>
2018-07-19 17:31:36 +0700

    CB countdown /Andry

--- controllers.js
+++ controllers.js
@@ -2,7 +2,13 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
-};
+  let interval = setInterval(function(){
+    displayCountdown(seconds)
+    seconds--
+    if (seconds < 0) {
+    clearInterval(interval)
+  }}, 1000)
+}
 
 module.exports = {
   timer
