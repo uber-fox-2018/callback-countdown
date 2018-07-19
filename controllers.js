5a3752c8e1a73e34da19a5e5342d0353697f6d82
Your Name <fahrisyany@gmail.com>
2018-07-19 16:14:40 +0700

    done

--- controllers.js
+++ controllers.js
@@ -2,6 +2,20 @@ const { displayCountdown } = require("./views");
 
 const timer = seconds => {
   // Your code here...
+  setInterval(function() {
+    if (seconds > 0) {
+      seconds -= 1;
+
+      let hour = Math.floor(seconds / 3600);
+      let minute = Math.floor((seconds % 3600) / 60);
+      let second = Math.floor(seconds - hour * 3600 - minute * 60) ;
+      let time = `${hour} : ${minute} : ${second}`;
+
+      displayCountdown(time);
+    } else {
+      clearInterval(seconds);
+    }
+  }, 1000);
 };
 
 module.exports = {
