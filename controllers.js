b648e704d427a88ae6a1107caae9a71b3f1cc75a
Susan Nio <susan.wen88@gmail.com>
2018-07-19 16:12:33 +0700

    Adding controllers.js

--- controllers.js
+++ controllers.js
@@ -1,9 +1,17 @@
 const { displayCountdown } = require("./views");
-
 const timer = seconds => {
   // Your code here...
+  var countDown = setInterval(function(){
+    console.log(seconds);
+    seconds--
+    displayCountdown(seconds);
+    if (seconds === 0) {
+      // console.log("HAPPY CODING!!");
+      clearInterval(countDown);
+    }
+  }, 1000);
 };
-
 module.exports = {
   timer
 };
+
